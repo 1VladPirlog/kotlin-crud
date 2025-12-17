2e30c8c21da0b516b13ec389b23aef7c7ad02be4
1VladPirlog <vladpirlog@ymail.com>
2025-12-17 14:38:24 +0200

    Run Ktor server with Gradle toolchain

---
+++ app/src/main/kotlin/App.kt
@@ -0,0 +1,19 @@
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.serialization.kotlinx.json.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080) {
+        install(ContentNegotiation) { json() }
+
+        routing {
+            get("/") {
+                call.respond(mapOf("status" to "ok"))
+            }
+        }
+    }.start(wait = true)
+}
