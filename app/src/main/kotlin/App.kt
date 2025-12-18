ed75976791fbacad0c86c9f8aeff3d33ad335e9a
1VladPirlog <vladpirlog@ymail.com>
2025-12-18 17:24:34 +0200

    added the GET functionality and routing

--- app/src/main/kotlin/App.kt
+++ app/src/main/kotlin/App.kt
@@ -1,19 +1,18 @@
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.routing.*
+import routes.healthRoutes
 
 fun main() {
     embeddedServer(Netty, port = 8080) {
         install(ContentNegotiation) { json() }
 
         routing {
-            get("/") {
-                call.respond(mapOf("status" to "ok"))
-            }
+            healthRoutes()
         }
     }.start(wait = true)
 }
+
